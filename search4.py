4d7e3adf60070afc4368ecf217927efb23b81293
Ruben Lopez Matias <47232476+RubenLopez3@users.noreply.github.com>
2022-05-12 02:54:12 -0400

    Create search4.py

---
+++ search4.py
@@ -0,0 +1,535 @@
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+
+
+class Problem(object):
+    """The abstract class for a formal problem. A new domain subclasses this,
+    overriding `actions` and `results`, and perhaps other methods.
+    The default heuristic is 0 and the default action cost is 1 for all states.
+    When yiou create an instance of a subclass, specify `initial`, and `goal` states
+    (or give an `is_goal` method) and perhaps other keyword args for the subclass."""
+
+    def __init__(self, initial=None, goal=None, **kwds):
+        self.__dict__.update(initial=initial, goal=goal, **kwds)
+
+    def actions(self, state):        raise NotImplementedError
+
+    def result(self, state, action): raise NotImplementedError
+
+    def is_goal(self, state):        return state == self.goal
+
+    def action_cost(self, s, a, s1): return 1
+
+    def h(self, node):               return 0
+
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(
+            type(self).__name__, self.initial, self.goal)
+
+
+class Node:
+    "A Node in a search tree."
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
+
+    def __repr__(self): return '<{}>'.format(self.state)
+
+    def __len__(self): return 0 if self.parent is None else (1 + len(self.parent))
+
+    def __lt__(self, other): return self.path_cost < other.path_cost
+
+
+failure = Node('failure', path_cost=math.inf)  # Indicates an algorithm couldn't find a solution.
+cutoff = Node('cutoff', path_cost=math.inf)  # Indicates iterative deepening search was cut off.
+
+
+def expand(problem, node):
+    "Expand a node, generating the children nodes."
+    s = node.state
+    for action in problem.actions(s):
+        s1 = problem.result(s, action)
+        cost = node.path_cost + problem.action_cost(s, action, s1)
+        yield Node(s1, node, action, cost)
+
+
+def path_actions(node):
+    "The sequence of actions to get to this node."
+    if node.parent is None:
+        return []
+    return path_actions(node.parent) + [node.action]
+
+
+def path_states(node):
+    "The sequence of states to get to this node."
+    if node in (cutoff, failure, None):
+        return []
+    return path_states(node.parent) + [node.state]
+
+
+## ---------QUEUES-----------------------------------------------------
+FIFOQueue = deque
+
+LIFOQueue = list
+
+
+class PriorityQueue:
+    """A queue in which the item with minimum f(item) is always popped first."""
+
+    def __init__(self, items=(), key=lambda x: x):
+        self.key = key
+        self.items = []  # a heap of (score, item) pairs
+        for item in items:
+            self.add(item)
+
+    def add(self, item):
+        """Add item to the queuez."""
+        pair = (self.key(item), item)
+        heapq.heappush(self.items, pair)
+
+    def pop(self):
+        """Pop and return the item with min f(item) value."""
+        return heapq.heappop(self.items)[1]
+
+    def top(self): return self.items[0][1]
+
+    def __len__(self): return len(self.items)
+
+
+# ------SEARCH ALGORTIHMS: BEST-FIRST-------------------------------------
+def best_first_search(problem, f):
+    "Search nodes with minimum f(node) value first."
+    node = Node(problem.initial)
+    frontier = PriorityQueue([node], key=f)
+    reached = {problem.initial: node}
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem, node):
+            s = child.state
+            if s not in reached or child.path_cost < reached[s].path_cost:
+                reached[s] = child
+                frontier.add(child)
+    return failure
+
+
+def best_first_tree_search(problem, f):
+    "A version of best_first_search without the `reached` table."
+    frontier = PriorityQueue([Node(problem.initial)], key=f)
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem, node):
+            if not is_cycle(child):
+                frontier.add(child)
+    return failure
+
+
+def g(n): return n.path_cost
+
+
+def astar_search(problem, h=None):
+    """Search nodes with minimum f(n) = g(n) + h(n)."""
+    h = h or problem.h
+    return best_first_search(problem, f=lambda n: g(n) + h(n))
+
+
+def astar_tree_search(problem, h=None):
+    """Search nodes with minimum f(n) = g(n) + h(n), with no `reached` table."""
+    h = h or problem.h
+    return best_first_tree_search(problem, f=lambda n: g(n) + h(n))
+
+
+def weighted_astar_search(problem, h=None, weight=1.4):
+    """Search nodes with minimum f(n) = g(n) + weight * h(n)."""
+    h = h or problem.h
+    return best_first_search(problem, f=lambda n: g(n) + weight * h(n))
+
+
+def greedy_bfs(problem, h=None):
+    """Search nodes with minimum h(n)."""
+    h = h or problem.h
+    return best_first_search(problem, f=h)
+
+
+def uniform_cost_search(problem):
+    "Search nodes with minimum path cost first."
+    return best_first_search(problem, f=g)
+
+
+def breadth_first_bfs(problem):
+    "Search shallowest nodes in the search tree first; using best-first."
+    return best_first_search(problem, f=len)
+
+
+def depth_first_bfs(problem):
+    "Search deepest nodes in the search tree first; using best-first."
+    return best_first_search(problem, f=lambda n: -len(n))
+
+
+def is_cycle(node, k=30):
+    "Does this node form a cycle of length k or less?"
+
+    def find_cycle(ancestor, k):
+        return (ancestor is not None and k > 0 and
+                (ancestor.state == node.state or find_cycle(ancestor.parent, k - 1)))
+
+    return find_cycle(node.parent, k)
+
+
+# -------OTHER SEARCH ALGORITHMS-----------------------------------------
+def breadth_first_search(problem):
+    "Search shallowest nodes in the search tree first."
+    node = Node(problem.initial)
+    if problem.is_goal(problem.initial):
+        return node
+    frontier = FIFOQueue([node])
+    reached = {problem.initial}
+    while frontier:
+        node = frontier.pop()
+        for child in expand(problem, node):
+            s = child.state
+            if problem.is_goal(s):
+                return child
+            if s not in reached:
+                reached.add(s)
+                frontier.appendleft(child)
+    return failure
+
+
+def iterative_deepening_search(problem):
+    "Do depth-limited search with increasing depth limits."
+    for limit in range(1, sys.maxsize):
+        result = depth_limited_search(problem, limit)
+        if result != cutoff:
+            return result
+
+
+def depth_limited_search(problem, limit=10):
+    "Search deepest nodes in the search tree first."
+    frontier = LIFOQueue([Node(problem.initial)])
+    result = failure
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        elif len(node) >= limit:
+            result = cutoff
+        elif not is_cycle(node):
+            for child in expand(problem, node):
+                frontier.append(child)
+    return result
+
+
+def depth_first_recursive_search(problem, node=None):
+    if node is None:
+        node = Node(problem.initial)
+    if problem.is_goal(node.state):
+        return node
+    elif is_cycle(node):
+        return failure
+    else:
+        for child in expand(problem, node):
+            result = depth_first_recursive_search(problem, child)
+            if result:
+                return result
+        return failure
+
+
+# path_states(depth_first_recursive_search(r2))
+
+# --------BIDIRECTIONAL BEST-FIRST SEARCH----------------------------------
+def bidirectional_best_first_search(problem_f, f_f, problem_b, f_b, terminated):
+    node_f = Node(problem_f.initial)
+    node_b = Node(problem_f.goal)
+    frontier_f, reached_f = PriorityQueue([node_f], key=f_f), {node_f.state: node_f}
+    frontier_b, reached_b = PriorityQueue([node_b], key=f_b), {node_b.state: node_b}
+    solution = failure
+    while frontier_f and frontier_b and not terminated(solution, frontier_f, frontier_b):
+        def S1(node, f):
+            return str(int(f(node))) + ' ' + str(path_states(node))
+
+        print('Bi:', S1(frontier_f.top(), f_f), S1(frontier_b.top(), f_b))
+        if f_f(frontier_f.top()) < f_b(frontier_b.top()):
+            solution = proceed('f', problem_f, frontier_f, reached_f, reached_b, solution)
+        else:
+            solution = proceed('b', problem_b, frontier_b, reached_b, reached_f, solution)
+    return solution
+
+
+def inverse_problem(problem):
+    if isinstance(problem, CountCalls):
+        return CountCalls(inverse_problem(problem._object))
+    else:
+        inv = problem.copy(problem)
+        inv.initial, inv.goal = inv.goal, inv.initial
+        return inv
+
+
+def bidirectional_uniform_cost_search(problem_f):
+    def terminated(solution, frontier_f, frontier_b):
+        n_f, n_b = frontier_f.top(), frontier_b.top()
+        return g(n_f) + g(n_b) > g(solution)
+
+    return bidirectional_best_first_search(problem_f, g, inverse_problem(problem_f), g, terminated)
+
+
+def bidirectional_astar_search(problem_f):
+    def terminated(solution, frontier_f, frontier_b):
+        nf, nb = frontier_f.top(), frontier_b.top()
+        return g(nf) + g(nb) > g(solution)
+
+    problem_b = inverse_problem(problem_f)
+    return bidirectional_best_first_search(problem_f, lambda n: g(n) + problem_f.h(n),
+                                           problem_b, lambda n: g(n) + problem_b.h(n),
+                                           terminated)
+
+
+def proceed(direction, problem, frontier, reached, reached2, solution):
+    node = frontier.pop()
+    for child in expand(problem, node):
+        s = child.state
+        print('proceed', direction, S(child))
+        if s not in reached or child.path_cost < reached[s].path_cost:
+            frontier.add(child)
+            reached[s] = child
+            if s in reached2:  # Frontiers collide; solution found
+                solution2 = (join_nodes(child, reached2[s]) if direction == 'f' else
+                             join_nodes(reached2[s], child))
+                # print('solution', path_states(solution2), solution2.path_cost,
+                # path_states(child), path_states(reached2[s]))
+                if solution2.path_cost < solution.path_cost:
+                    solution = solution2
+    return solution
+
+
+S = path_states
+
+
+# A-S-R + B-P-R => A-S-R-P + B-P
+def join_nodes(nf, nb):
+    """Join the reverse of the backward node nb to the forward node nf."""
+    # print('join', S(nf), S(nb))
+    join = nf
+    while nb.parent is not None:
+        cost = join.path_cost + nb.path_cost - nb.parent.path_cost
+        join = Node(nb.parent.state, join, nb.action, cost)
+        nb = nb.parent
+        # print('  now join', S(join), 'with nb', S(nb), 'parent', S(nb.parent))
+    return join
+
+
+# A , B = uniform_cost_search(r1), uniform_cost_search(r2)
+# path_states(A), path_states(B)
+
+# path_states(append_nodes(A, B))
+
+
+# ---------PROBLEM DOMAINS----------------------------------------------------
+
+
+# --------ROUTE FINDING PROBLEMS---------------------------------------------
+
+class RouteProblem(Problem):
+    """A problem to find a route between locations on a `Map`.
+    Create a problem with RouteProblem(start, goal, map=Map(...)}).
+    States are the vertexes in the Map graph; actions are destination states."""
+
+    def actions(self, state):
+        """The places neighboring `state`."""
+        return self.map.neighbors[state]
+
+    def result(self, state, action):
+        """Go to the `action` place, if the map says that is possible."""
+        return action if action in self.map.neighbors[state] else state
+
+    def action_cost(self, s, action, s1):
+        """The distance (cost) to go from s to s1."""
+        return self.map.distances[s, s1]
+
+    def h(self, node):
+        "Straight-line distance between state and the goal."
+        locs = self.map.locations
+        return straight_line_distance(locs[node.state], locs[self.goal])
+
+
+def straight_line_distance(A, B):
+    "Straight-line distance between two points."
+    return sum(abs(a - b) ** 2 for (a, b) in zip(A, B)) ** 0.5
+
+
+class Map:
+    """A map of places in a 2D world: a graph with vertexes and links between them.
+    In `Map(links, locations)`, `links` can be either [(v1, v2)...] pairs,
+    or a {(v1, v2): distance...} dict. Optional `locations` can be {v1: (x, y)}
+    If `directed=False` then for every (v1, v2) link, we add a (v2, v1) link."""
+
+    def __init__(self, links, locations=None, directed=False):
+        if not hasattr(links, 'items'):  # Distances are 1 by default
+            links = {link: 1 for link in links}
+        if not directed:
+            for (v1, v2) in list(links):
+                links[v2, v1] = links[v1, v2]
+        self.distances = links
+        self.neighbors = multimap(links)
+        self.locations = locations or defaultdict(lambda: (0, 0))
+
+
+def multimap(pairs) -> dict:
+    "Given (key, val) pairs, make a dict of {key: [val,...]}."
+    result = defaultdict(list)
+    for key, val in pairs:
+        result[key].append(val)
+    return result
+
+
+# Some specific RouteProblems
+
+# romania = Map(
+#     {('O', 'Z'):  71, ('O', 'S'): 151, ('A', 'Z'): 75, ('A', 'S'): 140, ('A', 'T'): 118,
+#      ('L', 'T'): 111, ('L', 'M'):  70, ('D', 'M'): 75, ('C', 'D'): 120, ('C', 'R'): 146,
+#      ('C', 'P'): 138, ('R', 'S'):  80, ('F', 'S'): 99, ('B', 'F'): 211, ('B', 'P'): 101,
+#      ('B', 'G'):  90, ('B', 'U'):  85, ('H', 'U'): 98, ('E', 'H'):  86, ('U', 'V'): 142,
+#      ('I', 'V'):  92, ('I', 'N'):  87, ('P', 'R'): 97},
+#     {'A': ( 76, 497), 'B': (400, 327), 'C': (246, 285), 'D': (160, 296), 'E': (558, 294),
+#      'F': (285, 460), 'G': (368, 257), 'H': (548, 355), 'I': (488, 535), 'L': (162, 379),
+#      'M': (160, 343), 'N': (407, 561), 'O': (117, 580), 'P': (311, 372), 'R': (227, 412),
+#      'S': (187, 463), 'T': ( 83, 414), 'U': (471, 363), 'V': (535, 473), 'Z': (92, 539)})
+
+
+# r0 = RouteProblem('A', 'A', map=romania)
+# r1 = RouteProblem('A', 'B', map=romania)
+# r2 = RouteProblem('N', 'L', map=romania)
+# r3 = RouteProblem('E', 'T', map=romania)
+# r4 = RouteProblem('O', 'M', map=romania)
+
+
+# ----GRID PROBLEMS---------------------------
+
+class GridProblem(Problem):
+    """Finding a path on a 2D grid with obstacles. Obstacles are (x, y) cells."""
+
+    def __init__(self, initial=(15, 30), goal=(130, 30), obstacles=(), **kwds):
+        Problem.__init__(self, initial=initial, goal=goal,
+                         obstacles=set(obstacles) - {initial, goal}, **kwds)
+
+    directions = [(-1, -1), (0, -1), (1, -1),
+                  (-1, 0), (1, 0),
+                  (-1, +1), (0, +1), (1, +1)]
+
+    def action_cost(self, s, action, s1): return straight_line_distance(s, s1)
+
+    def h(self, node): return straight_line_distance(node.state, self.goal)
+
+    def result(self, state, action):
+        "Both states and actions are represented by (x, y) pairs."
+        return action if action not in self.obstacles else state
+
+    def actions(self, state):
+        """You can move one cell in any of `directions` to a non-obstacle cell."""
+        x, y = state
+        return {(x + dx, y + dy) for (dx, dy) in self.directions} - self.obstacles
+
+
+class ErraticVacuum(Problem):
+    def actions(self, state):
+        return ['suck', 'forward', 'backward']
+
+    def results(self, state, action): return self.table[action][state]
+
+    table = dict(suck={1: {5, 7}, 2: {4, 8}, 3: {7}, 4: {2, 4}, 5: {1, 5}, 6: {8}, 7: {3, 7}, 8: {6, 8}},
+                 forward={1: {2}, 2: {2}, 3: {4}, 4: {4}, 5: {6}, 6: {6}, 7: {8}, 8: {8}},
+                 backward={1: {1}, 2: {1}, 3: {3}, 4: {3}, 5: {5}, 6: {5}, 7: {7}, 8: {7}})
+
+
+# --------plot---------------------------------------------------------------------
+def best_first_search(problem, f):
+    "Search nodes with minimum f(node) value first."
+    global reached  # <<<<<<<<<<< Only change here
+    node = Node(problem.initial)
+    frontier = PriorityQueue([node], key=f)
+    reached = {problem.initial: node}
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem, node):
+            s = child.state
+            if s not in reached or child.path_cost < reached[s].path_cost:
+                reached[s] = child
+                frontier.add(child)
+    return failure
+
+
+def plot_grid_problem(grid, solution, reached=(), title='Search', show=True):
+    "Use matplotlib to plot the grid, obstacles, solution, and reached."
+    reached = list(reached)
+    plt.figure(figsize=(16, 10))
+    plt.axis('off');
+    plt.axis('equal')
+    plt.scatter(*transpose(grid.obstacles), marker='s', color='darkgrey')
+    plt.scatter(*transpose(reached), 1 ** 2, marker='.', c='blue')
+    plt.scatter(*transpose(path_states(solution)), marker='s', c='blue')
+    plt.scatter(*transpose([grid.initial]), 9 ** 2, marker='D', c='green')
+    plt.scatter(*transpose([grid.goal]), 9 ** 2, marker='8', c='red')
+    if show: plt.show()
+    print('{} {} search: {:.1f} path cost, {:,d} states reached'
+          .format(' ' * 10, title, solution.path_cost, len(reached)))
+
+
+def plots(grid, weights=(1.4, 2)):
+    """Plot the results of 4 heuristic search algorithms for this grid."""
+    solution = astar_search(grid)
+    plot_grid_problem(grid, solution, reached, 'A* search')
+    for weight in weights:
+        solution = weighted_astar_search(grid, weight=weight)
+        plot_grid_problem(grid, solution, reached, '(b) Weighted ({}) A* search'.format(weight))
+    solution = greedy_bfs(grid)
+    plot_grid_problem(grid, solution, reached, 'Greedy best-first search')
+
+
+def transpose(matrix): return list(zip(*matrix))
+
+
+# -------- Statistics--------------------------------------------------------------
+class CountCalls:
+    """Delegate all attribute gets to the object, and count them in ._counts"""
+
+    def __init__(self, obj):
+        self._object = obj
+        self._counts = Counter()
+
+    def __getattr__(self, attr):
+        "Delegate to the original object, after incrementing a counter."
+        self._counts[attr] += 1
+        return getattr(self._object, attr)
+
+
+def report(searchers, problems, verbose=True):
+    """Show summary statistics for each searcher (and on each problem unless verbose is false)."""
+    for searcher in searchers:
+        print(searcher.__name__ + ':')
+        total_counts = Counter()
+        for p in problems:
+            prob = CountCalls(p)
+            soln = searcher(prob)
+            counts = prob._counts;
+            counts.update(actions=len(soln), cost=soln.path_cost)
+            total_counts += counts
+            if verbose: report_counts(counts, str(p)[:40])
+        report_counts(total_counts, 'TOTAL\n')
+
+
+def report_counts(counts, name):
+    """Print one line of the counts report."""
+    print('{:9,d} nodes |{:9,d} goal |{:5.0f} cost |{:8,d} actions | {}'.format(
+        counts['result'], counts['is_goal'], counts['cost'], counts['actions'], name))
+
